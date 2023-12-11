fd3ef7699bbd50d3608c665083b2ae248a4237cb
tronlegacy-agent <hoffman4you@gmail.com>
2023-12-11 15:31:54 +0800

    20231211

--- index.py
+++ index.py
@@ -163,15 +163,7 @@ def searchQ():
     else:  
         return render_template("input.html")
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    # build a request object
-    req = request.get_json(force=True)
-    # fetch queryResult from json
-    action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
-    return make_response(jsonify({"fulfillmentText": info}))
+
 
 
 
