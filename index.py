35c7d559d1c5c4d99fb828af6477d0d5ad4a1472
tronlegacy-agent <hoffman4you@gmail.com>
2023-12-11 15:30:15 +0800

    20231211

--- index.py
+++ index.py
@@ -173,59 +173,6 @@ def webhook():
     info = "動作：" + action + "； 查詢內容：" + msg
     return make_response(jsonify({"fulfillmentText": info}))
 
-@app.route("/movie_rate")
-def movie_rate():
-    url = "http://www.atmovies.com.tw/movie/next/"
-    Data = requests.get(url)
-    Data.encoding = "utf-8"
-    sp = BeautifulSoup(Data.text, "html.parser")
-    result=sp.select(".filmListAllX li")
-    lastUpdate = sp.find(class_="smaller09").text[5:]
-
-    for x in result:
-        picture = x.find("img").get("src").replace(" ", "")
-        title = x.find("img").get("alt")    
-        movie_id = x.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
-        hyperlink = "http://www.atmovies.com.tw" + x.find("a").get("href")
-
-        t = x.find(class_="runtime").text
-        showDate = t[5:15]
-
-        showLength = ""
-        if "片長" in t:
-            t1 = t.find("片長")
-            t2 = t.find("分")
-            showLength = t[t1+3:t2]
-
-        r = x.find(class_="runtime").find("img")
-        rate = ""
-        if r != None:
-            rr = r.get("src").replace("/images/cer_", "").replace(".gif", "")
-            if rr == "G":
-                rate = "普遍級"
-            elif rr == "P":
-                rate = "保護級"
-            elif rr == "F2":
-                rate = "輔12級"
-            elif rr == "F5":
-                rate = "輔15級"
-            else:
-                rate = "限制級"
-
-        doc = {
-            "title": title,
-            "picture": picture,
-            "hyperlink": hyperlink,
-            "showDate": showDate,
-            "showLength": showLength,
-            "rate": rate,
-            "lastUpdate": lastUpdate
-        }
-
-        db = firestore.client()
-        doc_ref = db.collection("電影含分級").document(movie_id)
-        doc_ref.set(doc)
-    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
 
 
 		
